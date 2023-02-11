6b93c10c219fc42673cdc45c0cd4d4810f219346
Daniel Kusai <dan.kusai@gmail.com>
2023-02-11 02:45:31 +0200

    moved all the different viruses to one file

--- ex4/Virus.h
+++ ex4/Virus.h
@@ -1,5 +1,4 @@
-#ifndef VIRUS_H
-#define VIRUS_H
+#pragma once
 
 #include <iostream>
 #include <string>
@@ -39,5 +38,49 @@ class Virus
 
     friend std::ostream& operator<<(std::ostream& out, const Virus& virus);
 };
+class Lentivirus : public Virus
+{
+  public:
+    Lentivirus() = delete;
+    Lentivirus(std::string name, int* genome, int viruslength);
+    Lentivirus(Virus& other);
+    ~Lentivirus() override {}
 
-#endif // VIRUS_H
+    virtual void print(std::ostream& out) const override;
+
+    virtual void operator*() override
+    {
+        for (int i = 0; i < 2; ++i)
+            Virus::operator*();
+    }
+};
+class Mimivirus : public Virus
+{
+  public:
+    Mimivirus() = delete;
+    Mimivirus(std::string name, int* genome, int viruslength);
+    Mimivirus(Virus& other);
+    ~Mimivirus() override;
+
+    virtual void print(std::ostream& out) const override;
+
+    virtual void operator*() override
+    {
+        for (int i = 0; i < 4; ++i)
+            Virus::operator*();
+    }
+};
+
+class Papilloma : public Virus
+{
+
+  public:
+    Papilloma() = delete;
+    Papilloma(std::string name, int* genome, int viruslength);
+    Papilloma(Virus& other);
+    ~Papilloma() override;
+
+    virtual void print(std::ostream& out) const override;
+
+    virtual void operator*() override { Virus::operator*(); }
+};
